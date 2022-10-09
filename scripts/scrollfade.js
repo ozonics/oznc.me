99de9bd61d790cdc7311de93ee5cb2fd0f40a518
Jia <jiaronghu8@gmail.com>
2022-10-09 18:39:00 +0800

    v4

--- scripts/scrollfade.js
+++ scripts/scrollfade.js
@@ -1,6 +1,6 @@
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
+        // console.log(entry)
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
         } else {
@@ -11,3 +11,11 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+// let scrollText = document.querySelector(".scroll-text");
+
+// window.onscroll = () => {
+//     let pos = window.scrollY;
+//     // console.log(pos);
+//     scrollText.style.left =  `-${pos/2}px`;
+// }
